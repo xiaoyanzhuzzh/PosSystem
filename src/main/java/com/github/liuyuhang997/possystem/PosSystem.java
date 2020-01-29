8d2fea61895baed7c741dac3eca3cb6a73127416
Liu Yuhang <liuyuhang997@outlook.com>
2020-01-29 15:27:20 +0800

    [Yuhang] feat: PosSystem print shop name and time.

--- src/main/java/com/github/liuyuhang997/possystem/PosSystem.java
+++ src/main/java/com/github/liuyuhang997/possystem/PosSystem.java
@@ -1,16 +1,25 @@
 package com.github.liuyuhang997.possystem;
 
 import com.github.liuyuhang997.possystem.promotions.Promotion;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.lang.String.format;
 import static java.util.stream.Collectors.toList;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class PosSystem {
+
+    private String shopName;
 
     public Cart loadCart(String path) {
         Cart cart = new Cart();
@@ -32,5 +41,20 @@ public class PosSystem {
             e.printStackTrace();
         }
         return new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        PosSystem posSystem = new PosSystem(args[0]);
+        posSystem.checkout();
+    }
+
+    public void checkout() {
+        System.out.println(format("shop name: %s", shopName));
+        System.out.println(format("print time: %s", getPrintTime()));
+    }
+
+    protected String getPrintTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.now().format(formatter);
     }
 }
--- src/test/java/com/github/liuyuhang997/possystem/PosSystemTest.java
+++ src/test/java/com/github/liuyuhang997/possystem/PosSystemTest.java
@@ -2,27 +2,43 @@ package com.github.liuyuhang997.possystem;
 
 import com.github.liuyuhang997.possystem.utils.TestUtil;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 public class PosSystemTest {
     private String cartPath = TestUtil.getResourcePath("cart.txt");
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    private PosSystem posSystem;
+
+    @BeforeEach
+    void setUp() {
+        posSystem = new PosSystem("ShopTest");
+        System.setOut(new PrintStream(outContent));
+    }
 
     @AfterEach
     void emptyFile() throws IOException {
         TestUtil.deleteFile(cartPath);
         TestUtil.initFileWithContext(cartPath, new ArrayList<>());
+        System.setOut(originalOut);
     }
 
     @Test
     void should_return_empty_cart_when_pos_system_load_file() {
-        PosSystem posSystem = new PosSystem();
         Cart cart = posSystem.loadCart(cartPath);
 
         assertThat(cart.getItemsSize(), is(0d));
@@ -33,7 +49,6 @@ public class PosSystemTest {
         String oneItem = "ITEM000001";
         TestUtil.initFileWithContext(cartPath, Collections.singletonList(oneItem));
 
-        PosSystem posSystem = new PosSystem();
         Cart cart = posSystem.loadCart(cartPath);
 
         assertThat(cart.getItemsSize(), is(1d));
@@ -44,9 +59,23 @@ public class PosSystemTest {
         String oneItemWithNum = "ITEM000001-2";
         TestUtil.initFileWithContext(cartPath, Collections.singletonList(oneItemWithNum));
 
-        PosSystem posSystem = new PosSystem();
         Cart cart = posSystem.loadCart(cartPath);
 
         assertThat(cart.getItemsSize(), is(2d));
+    }
+
+    @Test
+    void should_print_shop_name_when_pos_system_running() {
+        PosSystem.main(new String[]{"Shop"});
+        assertThat(outContent.toString(), containsString("shop name: Shop"));
+    }
+
+    @Test
+    void should_print_time_when_pos_system_checkout() {
+        PosSystem posSystemTestTime = spy(PosSystem.class);
+        when(posSystemTestTime.getPrintTime()).thenReturn("2020-01-29 14:00:00");
+
+        posSystemTestTime.checkout();
+        assertThat(outContent.toString(), containsString("print time: 2020-01-29 14:00:00"));
     }
 }
